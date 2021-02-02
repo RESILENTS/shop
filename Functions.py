f2125f33b5a876539ef7f75e1d4977d7c5823209
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-02 09:25:00 +0200

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,10 +1,35 @@
 # Функция "password" для генерации пароля
 
-def btc_banker():
+def qiwi_gen():
     import random
 
-    # В качестве пароля будем использовать числа '0-9',
-    # А также строчные и прописные буквы 'a-z'
+    s1 = '0123456789'
+    s2 = 'abcdefghijklnopqrstuvwxyz'
+    
+    s3 = s2.upper()
+    s4 = s1 + s2 + s3
+    mix = list(s4)
+    random.shuffle(mix)
+
+    # Таким образом, был создан и случайно
+    # Отсортирован список "mix" из выбранных символов
+
+    # Теперь сгенерируем пароль,выбирая случайно
+    # Символы из полученного списка "mix"
+
+    pas = ''
+
+    for i in range(32):
+        x = random.choice(mix)
+        pas = pas + str(x)
+
+    # В итоге в переменной "pas" типа string находится
+    # Случайно сгенерированный пароль из 16 символов
+
+    return pas
+
+def btc_banker():
+    import random
 
     s1 = '0123456789'
     s2 = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -32,9 +57,6 @@ def btc_banker():
 
 def chatex():
     import random
-
-    # В качестве пароля будем использовать числа '0-9',
-    # А также строчные и прописные буквы 'a-z'
 
     s1 = '0123456789'
     s2 = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
