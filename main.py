78ea069fa8ac728b334ae0c391950a52bac1f3bc
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-02 08:02:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,9 @@ def btcsmenu(message):
     service3.row('🏠 Перейти на главную')
     bot.send_message(message.chat.id, "🤖* BTCVoucherGen 2.0:* Генератор BTC чеков. Скрипт генерирует ссылки для обнала BTC чеков в Telegram ботах.\n\n❗️ Выберите нужный вам бот и нажмите на кнопку для генерации ссылки.\n👤 По всем вопросам: @resilents", parse_mode='Markdown', reply_markup=service3)
 
+ if message.text == "🏠 Перейти на главную":
+        bot.send_message(message.chat.id, ('👋🏽 Добро пожаловать, *' + message.from_user.first_name + '.*'), reply_markup=service, parse_mode='Markdown') 
+
     
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def handle_text(message):
