ff3d0c8268d061c9ed500eb91667b4cf8c58c4d0
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-02 07:27:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ ADMIN_CHAT_ID = 641892529
 
 chat_ids_file = 'chat_ids.txt'
 
+service = telebot.types.ReplyKeyboardMarkup(True)
 service.row('🛠 Инструменты', 'ℹ️ Канал бота')
 service.row('📊 Статистика', '🛒 Маркет')
 
+service2 = telebot.types.ReplyKeyboardMarkup(True)
 service2.row('⚙️ Генераторы', '🔍 OSINT')
 service2.row('📥 Парсеры', '🔗 Разные')
 service2.row('🏠 Перейти на главную')
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    service = telebot.types.ReplyKeyboardMarkup(True)
-
     bot.send_message(message.chat.id, ('👋🏽 Добро пожаловать, *' + message.from_user.first_name + '.*'), reply_markup=service, parse_mode='Markdown')
     
 @bot.message_handler(func=lambda message: True, content_types=['text'])
@@ -32,7 +32,6 @@ def handle_text(message):
             bot.send_message(message.chat.id, '📊Статистика отображается в реальном времени📡!\nПользователей: ' + str(size) + '\nСервисов для RU🇷🇺: 30\nСервисов для UK🇺🇦: 30\nБот запущен: 29.03.2020')
 
     if message.text == "🛠 Инструменты":
-        service2 = telebot.types.ReplyKeyboardMarkup(True)
         bot.send_message(message.chat.id, ('👋🏽 Добро пожаловать, *' + message.from_user.first_name + '.*'), reply_markup=service2, parse_mode='Markdown') 
     
 bot.polling(none_stop=True)
