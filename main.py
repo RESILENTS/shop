c122d422e21fa070902214e15a9cb2fa5c20cbe4
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-02 08:03:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,9 +32,6 @@ def btcsmenu(message):
     service3.row('🏠 Перейти на главную')
     bot.send_message(message.chat.id, "🤖* BTCVoucherGen 2.0:* Генератор BTC чеков. Скрипт генерирует ссылки для обнала BTC чеков в Telegram ботах.\n\n❗️ Выберите нужный вам бот и нажмите на кнопку для генерации ссылки.\n👤 По всем вопросам: @resilents", parse_mode='Markdown', reply_markup=service3)
 
- if message.text == "🏠 Перейти на главную":
-        bot.send_message(message.chat.id, ('👋🏽 Добро пожаловать, *' + message.from_user.first_name + '.*'), reply_markup=service, parse_mode='Markdown') 
-
     
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def handle_text(message):
@@ -42,7 +39,9 @@ def handle_text(message):
         with open('chat_ids.txt') as f:
             size = sum(1 for _ in f)
             bot.send_message(message.chat.id, '📊Статистика отображается в реальном времени📡!\nПользователей: ' + str(size) + '\nСервисов для RU🇷🇺: 30\nСервисов для UK🇺🇦: 30\nБот запущен: 29.03.2020')
-
+ if message.text == "🏠 Перейти на главную":
+        bot.send_message(message.chat.id, ('👋🏽 Добро пожаловать, *' + message.from_user.first_name + '.*'), reply_markup=service, parse_mode='Markdown') 
+            
     if message.text == "🛠 Инструменты":
         bot.send_message(message.chat.id, ('👋🏽 Добро пожаловать, *' + message.from_user.first_name + '.*'), reply_markup=service2, parse_mode='Markdown') 
         
