c33297bdf26bb414d2a9e5d0288103df1b6ec88a
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-02 07:26:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ ADMIN_CHAT_ID = 641892529
 
 chat_ids_file = 'chat_ids.txt'
 
+service.row('🛠 Инструменты', 'ℹ️ Канал бота')
+service.row('📊 Статистика', '🛒 Маркет')
+
+service2.row('⚙️ Генераторы', '🔍 OSINT')
+service2.row('📥 Парсеры', '🔗 Разные')
+service2.row('🏠 Перейти на главную')
+
 @bot.message_handler(commands=['start'])
 def welcome(message):
     service = telebot.types.ReplyKeyboardMarkup(True)
-    service.row('🛠 Инструменты', 'ℹ️ Канал бота')
-    service.row('📊 Статистика', '🛒 Маркет')
+
     bot.send_message(message.chat.id, ('👋🏽 Добро пожаловать, *' + message.from_user.first_name + '.*'), reply_markup=service, parse_mode='Markdown')
     
 @bot.message_handler(func=lambda message: True, content_types=['text'])
@@ -26,15 +32,7 @@ def handle_text(message):
             bot.send_message(message.chat.id, '📊Статистика отображается в реальном времени📡!\nПользователей: ' + str(size) + '\nСервисов для RU🇷🇺: 30\nСервисов для UK🇺🇦: 30\nБот запущен: 29.03.2020')
 
     if message.text == "🛠 Инструменты":
-        keyboard = telebot.types.InlineKeyboardMarkup()
-        url_button = types.InlineKeyboardButton(text="⚙️ BTCVoucherGen [2.0]:", url="https://t.me/BTC_CHANGE_BOT?start=с_")
-        url2_button = types.InlineKeyboardButton(text="⚙️ QIWITokenGen&Check [1.0]", url="https://t.me/BTC_CHANGE_BOT?start=с_")
-        keyboard.add(url_button, url2_button)
-        bot.send_message(message.chat.id, "888", parse_mode='Markdown', disable_web_page_preview=True, reply_markup=keyboard)
-
-        if message.text == "Dump DB" and chat_id == ADMIN_CHAT_ID:
-            f = open('chat_ids.txt')
-            bot.send_document(message.chat.id, f)
-    
+        service2 = telebot.types.ReplyKeyboardMarkup(True)
+        bot.send_message(message.chat.id, ('👋🏽 Добро пожаловать, *' + message.from_user.first_name + '.*'), reply_markup=service2, parse_mode='Markdown') 
     
 bot.polling(none_stop=True)
