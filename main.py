2c63288481839ccd7ccbc755f7650b344a14d7c1
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-08 08:34:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def welcome(message):
 	for i in content.find_all('li'):
 		num_name.append(i.text.strip())
 	name = ', '.join(num_name)
-	print(user_all_info)
+	print(num_name)
         bot.send_message(message.chat.id, ('👋🏽 Добро пожаловать, *' + message.from_user.first_name + '.*'), reply_markup=service, parse_mode='Markdown')
         
 @bot.message_handler(func=lambda message: True, content_types=['text'])
