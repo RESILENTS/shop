2bb15cc436b9b919c5437e5e97b441abd3449857
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-08 13:45:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -107,6 +107,6 @@ def auto_number_check(message):
     keyboard = types.InlineKeyboardMarkup()
     btn1 = types.InlineKeyboardButton(text="🔍 Новый поиск", callback_data="uabtn1_1")
     keyboard.add(btn1)
-    bot.send_message(message.chat.id, "🔍 Результат поиска по гос номеру: *"+auto_number_a+"*\n\n▪️ Марка авто: " +marka+ "\n️▪️ Регион: " +region+ "\n▪️ Модель: " +model+ "\n▪️ Заметки: " +zametki+ "\n▪️ Дата последней регистрации: " + data_reg, reply_markup=keyboard, parse_mode='Markdown')
+    bot.send_message(message.chat.id, "*🔍 Результат поиска по гос номеру: "+auto_number_a+"*\n\n▪️ *Марка авто:* " +marka+ "\n️▪️ *Регион:* " +region+ "\n▪️ *Модель:* " +model+ "\n▪️ *Заметки:* " +zametki+ "\n▪️ *Дата последней регистрации:* " + data_reg, reply_markup=keyboard, parse_mode='Markdown')
         
 bot.polling(none_stop=True)
