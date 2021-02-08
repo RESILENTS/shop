a231d77255560dbce907ad249a1a818fcc005de0
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-08 08:45:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def welcome(message):
 				num_name.append(i.text.strip())
 			name = ', '.join(num_name)
 			user_all_info = name
-			print(user_all_info)
+			print(num_name)
         
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def any_msg(message):
