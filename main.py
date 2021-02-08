4b54ac453a17ba3a09ddcae5682602a1b7be872b
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-08 13:14:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def callback_inline(call):
         if call.data == "uabtn1_1":
             service1 = telebot.types.ReplyKeyboardMarkup(True)
             service1.row('🔍 Поиск 🚙', '🏠 На главную')
-            uabtn1_1_message = bot.send_message(chat_id=call.message.chat.id, text="🔍 Поиск информации о автомобиле по гос. номеру:\n\nℹ️ Отправь мне номер авто для проверки, пример номера *AA1234BB.*", reply_markup=service1, parse_mode='Markdown')
+            uabtn1_1_message = bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="🔍 Поиск информации о автомобиле по гос. номеру:\n\nℹ️ Отправь мне номер авто для проверки, пример номера *AA1234BB.*", reply_markup=service1, parse_mode='Markdown')
             bot.register_next_step_handler(uabtn1_1_message, auto_number_check)
 	
 		
