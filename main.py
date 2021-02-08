1908eb53511c77b83695857733944be053e25995
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-08 12:59:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,11 +34,6 @@ def get_car_model(message):
 def usernameSearch(self): 
     global username_check_a
     
-    twitterurl = 'https://twitter.com/' + username_check_a
-    twitterresponse = get(twitterurl, headers=headers)
-    if twitterresponse.status_code == 200:
-        twitter_i = bot.send_message(message.from_user.id, ' ❌ *Twitter:* Не найдено.', parse_mode='Markdown')
-    else:
         twitter_i = bot.send_message(message.from_user.id, ' ➖ *Twitter:* https://twitter.com/' + username_check_a, parse_mode='Markdown')
 	
 bot.polling(none_stop=True)
